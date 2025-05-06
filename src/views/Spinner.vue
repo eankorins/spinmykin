df826d67036e5b084de98668252beabdd6adc742
abc abc <abc@abc.abc>
2025-05-06 21:52:30 +0200

    a

--- src/views/Spinner.vue
+++ src/views/Spinner.vue
@@ -7,22 +7,21 @@ id: number,
     wowClass: string,
     color: string
     name: string,
-    selected: true, selected: true, role: Role,
-
+    selected:true, role: Role,
 }
 
 const specs = ref<Spec[]>([
     {
         id: 1,
         name: "Havoc",
-        selected: false, selected: true, role: "DPS",
+        selected: false, role: "DPS",
         wowClass: "Demon Hunter",
         color: "bg-dh-100"
     },
     {
         id: 2,
         name: "Vengence",
-        selected: true, selected: true, role: "TANK",
+        selected: false, role: "TANK",
         wowClass: "Demon Hunter",
         color: "bg-dh-100",
     },
@@ -31,259 +30,259 @@ const specs = ref<Spec[]>([
         wowClass: "Death Knight",
         color: "bg-dk-100",
         name: "Frost",
-        selected: true, selected: true, role: "DPS"
+        selected: true, role: "DPS"
     },
     {
         id: 4,
         wowClass: "Death Knight",
         color: "bg-dk-100",
         name: "Unholy",
-        selected: true, selected: true, role: "DPS"
+        selected: true, role: "DPS"
     },
     {
         id: 5,
         wowClass: "Death Knight",
         color: "bg-dk-100",
         name: "Vengence",
-        selected: true, selected: true, role: "TANK"
+        selected:true, role: "TANK"
     },
     {
         id: 6,
         wowClass: "Druid",
         color: "bg-druid-100",
         name: "Guardian",
-        selected: true, selected: true, role: "TANK"
+        selected:true, role: "TANK"
     },
     {
         id: 7,
         wowClass: "Druid",
         color: "bg-druid-100",
         name: "Feral",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 8,
         wowClass: "Druid",
         color: "bg-druid-100",
         name: "Balance",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 9,
         wowClass: "Druid",
         color: "bg-druid-100",
         name: "Restoration",
-        selected: true, selected: true, role: "HEALER"
+        selected:true, role: "HEALER"
     },
     {
         id: 10,
         wowClass: "evoker",
         color: "bg-evoker-100",
         name: "Preservation",
-        selected: true, selected: true, role: "HEALER"
+        selected:true, role: "HEALER"
     },
     {
         id: 11,
         wowClass: "evoker",
         color: "bg-evoker-100",
         name: "Devestation",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 12,
         wowClass: "evoker",
         color: "bg-evoker-100",
         name: "Augmentation",
-        selected: true, role: "SUPPORT"
+        selected: false, role: "SUPPORT"
     },
     {
         id: 13,
         wowClass: "Hunter",
         color: "bg-hunter-100",
         name: "Marksmanship",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 14,
         wowClass: "Hunter",
         color: "bg-hunter-100",
         name: "Beast Mater",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 15,
         wowClass: "Hunter",
         color: "bg-hunter-100",
         name: "Survival",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 16,
         wowClass: "Mage",
         color: "bg-mage-100",
         name: "Arcane",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 17,
         wowClass: "Mage",
         color: "bg-mage-100",
         name: "Fire",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 18,
         wowClass: "Mage",
         color: "bg-mage-100",
         name: "Frost",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 19,
         wowClass: "Monk",
         color: "bg-monk-100",
         name: "Brewmaster",
-        selected: true, selected: true, role: "TANK"
+        selected:true, role: "TANK"
     },
     {
         id: 20,
         wowClass: "Monk",
         color: "bg-monk-100",
         name: "Mistweaver",
-        selected: true, selected: true, role: "HEALER"
+        selected:true, role: "HEALER"
     },
     {
         id: 21,
         wowClass: "Monk",
         color: "bg-monk-100",
         name: "Windwalker",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 22,
         wowClass: "Paladin",
         color: "bg-paladin-100",
         name: "Holy",
-        selected: true, selected: true, role: "HEALER"
+        selected:true, role: "HEALER"
     },
     {
         id: 23,
         wowClass: "Paladin",
         color: "bg-paladin-100",
         name: "Protection",
-        selected: true, selected: true, role: "TANK"
+        selected:true, role: "TANK"
     },
     {
         id: 24,
         wowClass: "Paladin",
         color: "bg-paladin-100",
         name: "Retribution",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 25,
         wowClass: "Priest",
         color: "bg-priest-100",
         name: "Discipline",
-        selected: true, selected: true, role: "HEALER"
+        selected:true, role: "HEALER"
     },
     {
         id: 26,
         wowClass: "Priest",
         color: "bg-priest-100",
         name: "Holy",
-        selected: true, selected: true, role: "HEALER"
+        selected:true, role: "HEALER"
     },
     {
         id: 27,
         wowClass: "Priest",
         color: "bg-priest-100",
         name: "Shadow",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 28,
         wowClass: "Rouge",
         color: "bg-rogue-100",
         name: "Assassination",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 29,
         wowClass: "Rouge",
         color: "bg-rogue-100",
         name: "Outlaw",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 30,
         wowClass: "Rouge",
         color: "bg-rogue-100",
         name: "Subtelty",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 31,
         wowClass: "Shaman",
         color: "bg-shaman-100",
         name: "Elemental",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 32,
         wowClass: "Shaman",
         color: "bg-shaman-100",
         name: "Enhancement",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 33,
         wowClass: "Shaman",
         color: "bg-shaman-100",
         name: "Restoration",
-        selected: true, selected: true, role: "HEALER"
+        selected:true, role: "HEALER"
     },
     {
         id: 34,
         wowClass: "Warlock",
         color: "bg-warlock-100",
         name: "Affliction",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 35,
         wowClass: "Warlock",
         color: "bg-warlock-100",
         name: "Demonology",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 36,
         wowClass: "Warlock",
         color: "bg-warlock-100",
         name: "Destruction",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 37,
         wowClass: "Warrior",
         color: "bg-warrior-100",
         name: "Arms",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 38,
         wowClass: "Warrior",
         color: "bg-warrior-100",
         name: "Fury",
-        selected: true, selected: true, role: "DPS"
+        selected:true, role: "DPS"
     },
     {
         id: 39,
         wowClass: "Warrior",
         color: "bg-warrior-100",
         name: "Protection",
-        selected: true, selected: true, role: "TANK"
+        selected:true, role: "TANK"
     }
 ]);
 
