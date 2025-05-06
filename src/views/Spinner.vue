1fef536025ce5111973ff50da0f01259a329359c
abc abc <abc@abc.abc>
2025-05-06 21:50:59 +0200

    spinny

--- src/views/Spinner.vue
+++ src/views/Spinner.vue
@@ -1,348 +1,310 @@
 <script setup lang="ts">
-import { computed, ref, type Ref } from 'vue';
-import { FortuneWheel, type Data, type ImgParams } from 'vue3-fortune-wheel';
+import { computed, ref } from 'vue';
 
 type Role = "DPS" | "TANK" | "HEALER" | "SUPPORT";
 interface Spec {
+id: number,
     wowClass: string,
     color: string
-    classColor: string,
     name: string,
-    role: Role,
+    selected: true, selected: true, role: Role,
 
 }
 
-const colors = {
-    'dk': '#c41e3a',
-    'dh': '#a330c9',
-    'druid': '#ff7c0a',
-    'evoker': '#33937f',
-    'hunter': '#aad372',
-    'mage': '#3fc7eb',
-    'monk': '#00ff98',
-    'paladin': '#f48cba',
-    'priest': '#fff',
-    'rogue': '#fff468',
-    'shaman': '#0070dd',
-    'warlock': '#8788ee',
-    'warrior': '#c69b6d',
-}
-
-const specs: Spec[] = [
+const specs = ref<Spec[]>([
     {
+        id: 1,
         name: "Havoc",
-        role: "DPS",
+        selected: false, selected: true, role: "DPS",
         wowClass: "Demon Hunter",
-        color: "bg-dh-100",
-        classColor: "dh"
+        color: "bg-dh-100"
     },
     {
+        id: 2,
         name: "Vengence",
-        role: "TANK",
+        selected: true, selected: true, role: "TANK",
         wowClass: "Demon Hunter",
         color: "bg-dh-100",
-        classColor: "dh"
     },
     {
+        id: 3,
         wowClass: "Death Knight",
         color: "bg-dk-100",
         name: "Frost",
-        role: "DPS",
-        classColor: "dk"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 4,
         wowClass: "Death Knight",
         color: "bg-dk-100",
         name: "Unholy",
-        role: "DPS",
-        classColor: "dk"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 5,
         wowClass: "Death Knight",
         color: "bg-dk-100",
-        name: "Blood",
-        role: "TANK",
-        classColor: "dk"
+        name: "Vengence",
+        selected: true, selected: true, role: "TANK"
     },
     {
+        id: 6,
         wowClass: "Druid",
         color: "bg-druid-100",
         name: "Guardian",
-        role: "TANK",
-        classColor: "druid"
+        selected: true, selected: true, role: "TANK"
     },
     {
+        id: 7,
         wowClass: "Druid",
         color: "bg-druid-100",
         name: "Feral",
-        role: "DPS",
-        classColor: "druid"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 8,
         wowClass: "Druid",
         color: "bg-druid-100",
         name: "Balance",
-        role: "DPS",
-        classColor: "druid"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 9,
         wowClass: "Druid",
         color: "bg-druid-100",
         name: "Restoration",
-        role: "HEALER",
-        classColor: "druid"
+        selected: true, selected: true, role: "HEALER"
     },
     {
+        id: 10,
         wowClass: "evoker",
         color: "bg-evoker-100",
         name: "Preservation",
-        role: "HEALER",
-        classColor: "evoker"
+        selected: true, selected: true, role: "HEALER"
     },
     {
+        id: 11,
         wowClass: "evoker",
         color: "bg-evoker-100",
         name: "Devestation",
-        role: "DPS",
-        classColor: "evoker"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 12,
         wowClass: "evoker",
         color: "bg-evoker-100",
         name: "Augmentation",
-        role: "SUPPORT",
-        classColor: "evoker"
+        selected: true, role: "SUPPORT"
     },
     {
+        id: 13,
         wowClass: "Hunter",
         color: "bg-hunter-100",
         name: "Marksmanship",
-        role: "DPS",
-        classColor: "hunter"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 14,
         wowClass: "Hunter",
         color: "bg-hunter-100",
         name: "Beast Mater",
-        role: "DPS",
-        classColor: "hunter"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 15,
         wowClass: "Hunter",
         color: "bg-hunter-100",
         name: "Survival",
-        role: "DPS",
-        classColor: "hunter"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 16,
         wowClass: "Mage",
         color: "bg-mage-100",
         name: "Arcane",
-        role: "DPS",
-        classColor: "mage"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 17,
         wowClass: "Mage",
         color: "bg-mage-100",
         name: "Fire",
-        role: "DPS",
-        classColor: "mage"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 18,
         wowClass: "Mage",
         color: "bg-mage-100",
         name: "Frost",
-        role: "DPS",
-        classColor: "mage"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 19,
         wowClass: "Monk",
         color: "bg-monk-100",
         name: "Brewmaster",
-        role: "TANK",
-        classColor: "monk"
+        selected: true, selected: true, role: "TANK"
     },
     {
+        id: 20,
         wowClass: "Monk",
         color: "bg-monk-100",
         name: "Mistweaver",
-        role: "HEALER",
-        classColor: "monk"
+        selected: true, selected: true, role: "HEALER"
     },
     {
+        id: 21,
         wowClass: "Monk",
         color: "bg-monk-100",
         name: "Windwalker",
-        role: "DPS",
-        classColor: "monk"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 22,
         wowClass: "Paladin",
         color: "bg-paladin-100",
         name: "Holy",
-        role: "HEALER",
-        classColor: "paladin"
+        selected: true, selected: true, role: "HEALER"
     },
     {
+        id: 23,
         wowClass: "Paladin",
         color: "bg-paladin-100",
         name: "Protection",
-        role: "TANK",
-        classColor: "paladin"
+        selected: true, selected: true, role: "TANK"
     },
     {
+        id: 24,
         wowClass: "Paladin",
         color: "bg-paladin-100",
         name: "Retribution",
-        role: "DPS",
-        classColor: "paladin"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 25,
         wowClass: "Priest",
         color: "bg-priest-100",
         name: "Discipline",
-        role: "HEALER",
-        classColor: "priest"
+        selected: true, selected: true, role: "HEALER"
     },
     {
+        id: 26,
         wowClass: "Priest",
         color: "bg-priest-100",
         name: "Holy",
-        role: "HEALER",
-        classColor: "priest"
+        selected: true, selected: true, role: "HEALER"
     },
     {
+        id: 27,
         wowClass: "Priest",
         color: "bg-priest-100",
         name: "Shadow",
-        role: "DPS",
-        classColor: "priest"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 28,
         wowClass: "Rouge",
         color: "bg-rogue-100",
         name: "Assassination",
-        role: "DPS",
-        classColor: "rogue"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 29,
         wowClass: "Rouge",
         color: "bg-rogue-100",
         name: "Outlaw",
-        role: "DPS",
-        classColor: "rogue"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 30,
         wowClass: "Rouge",
         color: "bg-rogue-100",
         name: "Subtelty",
-        role: "DPS",
-        classColor: "rogue"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 31,
         wowClass: "Shaman",
         color: "bg-shaman-100",
         name: "Elemental",
-        role: "DPS",
-        classColor: "shaman"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 32,
         wowClass: "Shaman",
         color: "bg-shaman-100",
         name: "Enhancement",
-        role: "DPS",
-        classColor: "shaman"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 33,
         wowClass: "Shaman",
         color: "bg-shaman-100",
         name: "Restoration",
-        role: "HEALER",
-        classColor: "shaman"
+        selected: true, selected: true, role: "HEALER"
     },
     {
+        id: 34,
         wowClass: "Warlock",
         color: "bg-warlock-100",
         name: "Affliction",
-        role: "DPS",
-        classColor: "warlock"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 35,
         wowClass: "Warlock",
         color: "bg-warlock-100",
         name: "Demonology",
-        role: "DPS",
-        classColor: "warlock"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 36,
         wowClass: "Warlock",
         color: "bg-warlock-100",
         name: "Destruction",
-        role: "DPS",
-        classColor: "warlock"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 37,
         wowClass: "Warrior",
         color: "bg-warrior-100",
         name: "Arms",
-        role: "DPS",
-        classColor: "warrior"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 38,
         wowClass: "Warrior",
         color: "bg-warrior-100",
         name: "Fury",
-        role: "DPS",
-        classColor: "warrior"
+        selected: true, selected: true, role: "DPS"
     },
     {
+        id: 39,
         wowClass: "Warrior",
         color: "bg-warrior-100",
         name: "Protection",
-        role: "TANK",
-        classColor: "warrior"
+        selected: true, selected: true, role: "TANK"
     }
-];
+]);
 
 const highlighted = ref(-1);
 
-function shuffleArray(array: any[]) {
-    for (var i = array.length - 1; i >= 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
-const logo: ImgParams = {
-  src: 'https://logos-world.net/wp-content/uploads/2020/09/Oracle-Logo-700x394.png',
-  width: 100,
-  height: 120
-}
+
 const getRandom = function () {
-    let max = specs.length;
+    let max = selectedSpecs.value.length;
     let idx = Math.floor(Math.random() * max);
     return idx;
 }
-
-const spinWinnerId = ref(-1);
 const spinning = ref(false);
 const winner = computed(() => {
     return specs[highlighted.value];
 });
 
-const wheelData = computed(() => {
-    let i = 0;
-    let arr = [...specs];
-    shuffleArray(arr);
-    return arr.map(s => {
-        return {
-            id: i++, value: s.name, bgColor: (colors as any)[s.classColor], color: 'red'
-        }
-    })
-});
+const selectedSpecs = computed(() => {
+    return specs.value.filter(s => s.selected);    
+})
 const doSpin = async function (start: number, interval: number, max: number) {
-    highlighted.value = getRandom();
+    highlighted.value = selectedSpecs.value[getRandom()].id;
     console.log("Selected", highlighted);
     if (Date.now() - start < max) {
         setTimeout(() => { doSpin(start, interval + 20, max) }, interval);
@@ -351,10 +313,10 @@ const doSpin = async function (start: number, interval: number, max: number) {
     }
 }
 
-const spinWinner: Ref<Data |undefined> = ref();
-const done = function(result: Data){
-   spinWinner.value = result; 
-};
+const toggleSpec = (s, idx) => {
+    console.log(s);
+    s.selected = !s.selected;
+}
 
 const spin = async function () {
     let nextInterval = 50;
@@ -364,46 +326,29 @@ const spin = async function () {
     doSpin(start, nextInterval, maxTime);
 }
 
-const wheel = ref<InstanceType<typeof FortuneWheel> | null>(null)
-
-const launchWheel = () => {
-    spinWinner.value = undefined;
-  wheel.value?.spin()
-}
-
 </script>
 
 <template>
     <main>
-        <div class="w-[100vw] h-[100vh] bg-slate-900 flex">
-            <!-- <div class="h-full"> -->
-            <!--     <div class="flex flex-wrap gap-2 items-center w-[500px]"> -->
-            <!--         <div v-for="(s, idx) in specs" :key="s.name" -->
-            <!--             :class="[s.color, highlighted == idx ? 'highlighted' : '']" class="w-36 h-12"> -->
-            <!--             <div class="p-2">{{ s.name }}</div> -->
-            <!--         </div> -->
-            <!--     </div> -->
-            <!-- </div> -->
-            <!-- <div class="self-center"> -->
-            <!--     <div class="w-36 h-12" v-if="highlighted > -1"> -->
-            <!--         <div :class="winner.color" class="w-36 h-12" v-if="winner"> -->
-            <!--             <div class="p-2">{{ winner.name }}</div> -->
-            <!--         </div> -->
-            <!--     </div> -->
-            <!--     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" v-on:click="spin()" -->
-            <!--         v-if="!spinning"> -->
-            <!--         Spin -->
-            <!--     </button> -->
-            <!-- </div> -->
-            <div>
-                <FortuneWheel ref="wheel" v-model="spinWinnerId" @done="done" :data="wheelData" :img-params="logo"/>
-                <div class="w-36 h-12"  v-if="spinWinner">
-                    <div :style="{ background: spinWinner.bgColor}" class="w-36 h-12">
-                        <div class="p-2">{{ spinWinner.value }}</div>
+        <div class="w-[100vw] h-[100vh] bg-slate-900 flex justify-center pt-20">
+            <div class="h-full">
+                <div class="flex flex-wrap gap-2 items-center w-[500px]">
+                    <div v-for="(s, idx) in specs" :key="s.name"
+                        :class="[s.color, s.selected ? '' : 'opacity-40', highlighted == s.id ? 'highlighted' : '']"
+                        class="w-36 h-12 cursor-pointer" v-on:click="toggleSpec(s, idx)">
+                        <div class="p-2">{{ s.name }}</div>
                     </div>
                 </div>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" v-on:click="launchWheel()">
-                    Spin Wheel
+            </div>
+            <div class="self-center">
+                <div class="w-36 h-12" v-if="highlighted > -1">
+                    <div :class="winner.color" class="w-36 h-12" v-if="winner">
+                        <div class="p-2">{{ winner.name }}</div>
+                    </div>
+                </div>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" v-on:click="spin()"
+                    v-if="!spinning">
+                    Spin
                 </button>
             </div>
         </div>
@@ -419,3 +364,4 @@ input[type=button] {
     @apply bg-white;
 }
 </style>
+
